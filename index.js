5fe1047506cd72fa3093c2a63cad8286eb50df6f
raycewiggins <raycewiggins@gmail.com>
2017-11-11 19:12:11 -0600

    Implement passoport.js and google oauth

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-//route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-
+//creates a new instance of a Google Strategy
+passport.use(new GoogleStrategy());
 
 //allows heroku to dynamically set the port using the PORT env variable
 const PORT = process.env.PORT || 5000
