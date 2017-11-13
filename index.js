7d20da7d2ce099736be55f35eee6bd8280c0343b
raycewiggins <raycewiggins@gmail.com>
2017-11-12 19:27:25 -0600

    Implement oauth flow with passport

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
 //creates a new instance of a Google Strategy
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    } 
+  )
+);
+
+// google is an internal identifier for the GoogleStrategy
+// scope contains the permissions
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //allows heroku to dynamically set the port using the PORT env variable
 const PORT = process.env.PORT || 5000
