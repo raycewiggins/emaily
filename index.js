4194f9d910cae4e48837cd461abb4e6fa10e3406
raycewiggins <raycewiggins@gmail.com>
2017-11-18 17:41:52 -0600

    Implement cookie-session

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +9,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI)
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+// tells passport to use cookie authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // when require is called it returns a function and immediately invokes the function
 require('./routes/authRoutes')(app);
