050b57ca38f177341344cd582f5a4824e4f5b296
raycewiggins <raycewiggins@gmail.com>
2017-11-18 12:42:16 -0600

    Wire app to mongodb (mlab)

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 //creates a new instance of a Google Strategy
 passport.use(
@@ -11,9 +14,8 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-      console.log(accessToken);
+      //creates a new instance of a user and saves it to the database
+      new User({ googleId: profile.id }).save();
     } 
   )
 );
