153da710791ff1f6f4d3099460e441328b3e158a
raycewiggins <raycewiggins@gmail.com>
2017-12-03 18:04:01 -0600

    Implement Materialize and create header component

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
-import reducers from './reducers'
+import reducers from './reducers';
 
 //first arg in create store are where the reducers go. {} = initial state (for server side rendering)
 const store = createStore(reducers, {}, applyMiddleware());
