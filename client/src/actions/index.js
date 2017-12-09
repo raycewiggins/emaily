3550ea46de9ee973cb4fcfd500b68ca9de8adb44
raycewiggins <raycewiggins@gmail.com>
2017-12-09 14:35:10 -0600

    Wire up actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 //redux thunk looks at what is returned from the action creator and automatically calls the internal function it with dispatch
-const fetchUser = () => {
+export const fetchUser = () => {
   return function(dispatch) {
     axios
       .get('/api/current_user')
