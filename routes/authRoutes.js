4938658d09be41b75c66576018df46f593e66a82
raycewiggins <raycewiggins@gmail.com>
2017-12-09 15:55:17 -0600

    Update logout routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,7 +21,7 @@ module.exports = (app) => {
   app.get('/api/logout', (req, res) => {
     // passport takes the cookie and deletes it
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   })
 
   app.get('/api/current_user', (req, res) => {
