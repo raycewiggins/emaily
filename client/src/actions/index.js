4aebd3df9f0dce866489c9e6a403c1258f024b4f
raycewiggins <raycewiggins@gmail.com>
2017-12-10 19:40:17 -0600

    Add handleToken action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+//where all the action creators are
+
 //redux thunk looks at what is returned from the action creator and automatically calls the internal function it with dispatch
 export const fetchUser = () => async dispatch => {
   //the output from axios
@@ -8,3 +10,9 @@ export const fetchUser = () => async dispatch => {
   
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
