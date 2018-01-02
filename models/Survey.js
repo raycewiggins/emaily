bbbe71eabb5c3db97d543f073d4106bda7e2e68c
raycewiggins <raycewiggins@gmail.com>
2018-01-01 18:59:21 -0600

    Add recipient subdoc schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const{ Schema } = mongoose;
+const RecipientScehma = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
-  recipients: [String]
+  recipients: [RecipientScehma],
+  yes: { type: Number, default: 0 },
+  no: { type: Number, default: 0 }
 });
 
 mongoose.model('surveys', surveySchema);
