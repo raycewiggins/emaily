bfb7589808acc0923590bef0ecc660c199cce685
raycewiggins <raycewiggins@gmail.com>
2018-01-12 19:35:15 -0600

    Complete mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,5 +3,54 @@ const helper = sendgrid.mail;
 const keys = require('../congig/keys');
 
 class Mailer extends helper.Mail {
-  
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Contect('text/html', content);
+    this.recipeints = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  // unwraps the comma separated string of emails to an array of emails
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipeints.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  // this sends of the mailer to sendGrid
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/semd',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
+  }
 };
+
+module.exports = Mailer;
