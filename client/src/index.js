4d1530b5be129e6cab34bdf6de5cd382b2673f33
raycewiggins <raycewiggins@gmail.com>
2018-01-14 19:51:56 -0600

    Refine email template

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// Development purposes only for testing email service
+import axios from 'axios';
+window.axios = axios;
+
 //first arg in create store are where the reducers go. {} = initial state (for server side rendering)
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
